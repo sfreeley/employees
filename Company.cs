4be951a332d7d41dc1b825aae38cd30f65d3eb63
Sisi Freeley <liusi33@gmail.com>
2020-08-27 22:08:38 -0500

    list employees working at company

---
+++ Company.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    public class Company
+    {
+
+        // Some readonly properties (let's talk about gets, baby)
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+
+        // Create a public property for holding a list of current employees
+        public List<Employee> Employees { get; set; }
+
+        /*
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
+            The constructor will set the value of the public properties
+        */
+
+        public Company(string name)
+        {
+            Name = name;
+            CreatedOn = DateTime.Now;
+            Employees = new List<Employee>();
+        }
+
+        public void AssignEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+
+        public void ListEmployees()
+        {
+            foreach (Employee employee in Employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of a company.
+            Company company1 = new Company("Parks Department");
+
+            //create three employees
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Leslie";
+            employee1.LastName = "Knope";
+            employee1.Title = "deputy director Parks and Rec";
+            employee1.StartDate = new DateTime(2010, 2, 1, 9, 0, 0);
+
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Ron";
+            employee2.LastName = "Swanson";
+            employee2.Title = "director Parks and Rec";
+            employee2.StartDate = new DateTime(2008, 3, 1, 7, 0, 0);
+
+            Employee employee3 = new Employee();
+            employee3.FirstName = "Andy";
+            employee3.LastName = "Dwyer";
+            employee3.Title = "the best";
+            employee3.StartDate = new DateTime(2016, 2, 1, 4, 0, 0);
+
+            company1.AssignEmployee(employee1);
+            company1.AssignEmployee(employee2);
+            company1.AssignEmployee(employee3);
+
+            company1.ListEmployees();
+
+        }
+    }
+}
